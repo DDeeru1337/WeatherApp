a6309de45987148ba38e900fc76208bacf7372ca
DDeer <ddeeru1337@gmail.com>
2024-01-13 21:27:10 +0300

    Добавить Data - DayItem

---
+++ app/src/main/java/com/example/weatherxmlpro/DayItem.kt
@@ -0,0 +1,10 @@
+package com.example.weatherxmlpro
+
+data class DayItem(
+    val city: String,
+    val time: String,
+    val temp: String,
+    val condition: String,
+    val iconUrl: String,
+    val hours: String
+)
