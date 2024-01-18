685aa3ba4295e13240a6f16dbde6c3dc6b4d3828
DDeer <ddeeru1337@gmail.com>
2024-01-18 21:36:10 +0300

    "Доделать fragment_main.xml и создать list_item.xml #8"

--- app/src/main/java/com/example/weatherxmlpro/DayItem.kt
+++ app/src/main/java/com/example/weatherxmlpro/DayItem.kt
@@ -2,7 +2,7 @@ package com.example.weatherxmlpro
 
 data class DayItem(
     val city: String,
-    val time: String,
+    val date: String,
     val temp: String,
     val condition: String,
     val iconUrl: String,
--- app/src/main/java/com/example/weatherxmlpro/MainViewModel.kt
+++ app/src/main/java/com/example/weatherxmlpro/MainViewModel.kt
@@ -4,6 +4,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<String>()
+    val liveDataCurrent = MutableLiveData<DayItem>()
+    val liveDataList = MutableLiveData<DayItem>()
 }
